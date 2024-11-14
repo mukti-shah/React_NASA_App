28918b820a9d5e1a9e57f2f90a1a8eeb3ad8b8ef
Mukti Shah <mukti.shah.050@gmail.com>
2024-11-14 16:16:54 +0530

    APOD with desired date

--- src/App.jsx
+++ src/App.jsx
@@ -10,47 +10,64 @@ function App() {
   
   const [showModal, setShowModal] = useState(false)
 
+  const today = new Date();
+  const formattedToday = today.toISOString().split('T')[0];
+
+  const [selectedDate, setSelectedDate] = useState(formattedToday);
+
   function handleDisplayModal(){
     setShowModal(!showModal)
+  }
+
+  function handleLoading(){
+    if (loading){
+      <div className="loadingState"><i className="fa-duotone fa-solid fa-gear"></i></div>
+    }
   }
 
   useEffect(()=> {
     async function fetchAPIData(){
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
-      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+      const url = 'https://api.nasa.gov/planetary/apod?'+ `start_date=${selectedDate}&`+`end_date=${selectedDate}&` + `api_key=${NASA_KEY}`
+      
+      // const localKey = `NASA-${today}`
 
-      const today = (new Date()).toDateString()
-      const localKey = `NASA-${today}`
+      // if (localStorage.getItem(localKey)){
+      //   const api_data = JSON.parse(localStorage.getItem(localKey))
+      //   setData(api_data)
+      //   return
+      // }
 
-      if (localStorage.getItem(localKey)){
-        const api_data = JSON.parse(localStorage.getItem(localKey))
-        setData(api_data)
-        return
-      }
+      // localStorage.clear()
 
-      localStorage.clear()
+      // setLoading(true)
+      // handleLoading()
+      setData('')
 
       try{
         const res = await fetch(url)
-        const api_data = await res.json()
+        const data1 = await res.json()
+        const api_data = data1[0]
         setData(api_data)
-        localStorage.setItem(localKey, JSON.stringify(api_data))
-        // console.log("data\n", api_data)
+        // localStorage.setItem(localKey, JSON.stringify(api_data))
+        console.log("data\n", api_data)
+        // setLoading(false)
+        // handleLoading()
       }
       catch (err){
         console.log(err.message)
       }
     }
     fetchAPIData()
-  }, [])
+  }, [selectedDate])
   
   return (
     <>
       {data ? (<Main data={data}/>) : (
-        <div className="loadingState"><i class="fa-duotone fa-solid fa-gear"></i></div>
+        <div className="loadingState"><i className="fa-duotone fa-solid fa-gear"></i></div>
       )}
       {showModal && (<SideBar data = {data} handleDisplayModal={handleDisplayModal} />)}
-      {data && (<Footer data = {data} handleDisplayModal={handleDisplayModal} />)}
+      {data && (<Footer data = {data} handleDisplayModal={handleDisplayModal} setSelectedDate={setSelectedDate} selectedDate={selectedDate} />)}
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,29 @@ import React from 'react'
 
 export default function Footer(props) {
 
-    const {handleDisplayModal, data} = props
+    const {handleDisplayModal, data, setSelectedDate, selectedDate} = props
+    
+    function handleDateChange() {
+      const newDate = document.getElementById("date").value;
+      console.log(newDate)
+      setSelectedDate(newDate);
+    }
 
   return (
     <footer>
         <div className="bgGradient"></div>
         <div>
-        <h1>APOD PROJECT</h1>
-            <h2>{data?.title}</h2>
+          <h1>APOD PROJECT</h1>
+          <h2>{data?.title}</h2>
         </div>
-        <button onClick={handleDisplayModal}><i className="fa-solid fa-circle-info"></i></button>
+        <div>
+          <button onClick={handleDisplayModal}><i className="fa-solid fa-circle-info"></i></button>
+          <div className='datePicker'>
+          <input type="date" name="date" id="date" max={new Date().toISOString().split('T')[0]}/>
+          <button onClick={handleDateChange}><i class="fa-solid fa-eye"></i></button>
+          </div>
+        </div>
+        
     </footer>
   )
 }
